d058dd75883e22817993bd5ed54369a57ca3a2b4
Ignacio Sanchez Gines <nacho@geardome.com>
2021-08-12 17:26:54 +0200

    update

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from boto3.session import Session
 from botocore.client import Config
 from botocore.handlers import set_list_objects_encoding_type_url
 
-import boto3
 import os
+import time
+import boto3
 
 s3_access_key = os.environ['S3_ACCESS_KEY']
 s3_secret_key = os.environ['S3_SECRET_KEY']
@@ -18,11 +19,15 @@ session.events.unregister('before-parameter-build.s3.ListObjects', set_list_obje
 
 s3 = session.resource('s3', endpoint_url=s3_host, config=Config(signature_version='s3v4'))
 
-buckets = s3.buckets.all()
-for bucket in buckets:
-    print("############### BUCKET ###############")
-    print(bucket)
-    print("+++++++++++++++ OBJECTS ++++++++++++++")
-    for f in bucket.objects.all():
-        print(f.key)
-    print("######################################")
+while True:
+    print(">> BEGIN")
+    buckets = s3.buckets.all()
+    for bucket in buckets:
+        print("#### BUCKET #####")
+        print(bucket)
+        print("++++ OBJECTS ++++")
+        for f in bucket.objects.all():
+            print(f.key)
+        print("#################")
+    print(">> END")
+    time.sleep(60)
