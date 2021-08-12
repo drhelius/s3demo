0be95ab63886e083aae1c62b021bc2d034f4302b
Ignacio Sanchez Gines <nacho@geardome.com>
2021-08-12 16:49:52 +0200

    update

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-import boto
-import boto.s3.connection
+from boto3.session import Session
+from botocore.client import Config
+from botocore.handlers import set_list_objects_encoding_type_url
+
+import boto3
 import os
 
-access_key = os.environ['ACCESS_KEY']
-secret_key = os.environ['SECRET_KEY']
+s3_access_key = os.environ['S3_ACCESS_KEY']
+s3_secret_key = os.environ['S3_SECRET_KEY']
 s3_host = os.environ['S3_HOST']
+s3_region = os.environ['S3_REGION']
 
-conn = boto.connect_s3(
-        aws_access_key_id = access_key,
-        aws_secret_access_key = secret_key,
-        host = s3_host,
-        calling_format = boto.s3.connection.OrdinaryCallingFormat(),
-        )
+#boto3.set_stream_logger('')
 
-for bucket in conn.get_all_buckets():
-        print("{name}\t{created}".format(
-                name = bucket.name,
-                created = bucket.creation_date,
-        ))
+session = Session(aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key, region_name=s3_region)
+
+session.events.unregister('before-parameter-build.s3.ListObjects', set_list_objects_encoding_type_url)
+
+s3 = session.resource('s3', endpoint_url=s3_host, config=Config(signature_version='s3v4'))
+
+buckets = s3.buckets.all()
+for bucket in buckets:
+    print("############### BUCKET ###############")
+    print(bucket)
+    print("+++++++++++++++ OBJECTS ++++++++++++++")
+    for f in bucket.objects.all():
+        print(f.key)
+    print("######################################")
